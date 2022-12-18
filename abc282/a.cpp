f14f2eb57748af3fb04dd3a335372670cc6e390a
北洞亜也加 <pocchi13.tr@gmail.com>
2022-12-18 21:37:16 +0900

    12/18

---
+++ abc282/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  int K;
+  cin >> K;
+
+  vector<string> A = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+  for (int i = 0; i < K; i++)
+  {
+    cout << A[i];
+  }
+
+  cout << endl;
+
+  return 0;
+}
