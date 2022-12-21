857da2ac2ef62829fbeb4e4589226f03c232b1a8
北洞亜也加 <pocchi13.tr@gmail.com>
2022-12-21 23:04:30 +0900

    12/21

---
+++ abc244/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+  int N;
+  string S;
+  cin >> N;
+  cin >> S;
+
+  int size = S.size();
+  cout << S[size - 1] << endl;
+
+  return 0;
+}
